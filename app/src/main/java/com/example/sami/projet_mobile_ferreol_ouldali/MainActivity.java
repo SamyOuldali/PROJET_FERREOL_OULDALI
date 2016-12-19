80d318e9cd0e8849b349c391d5398ac0e35783f0
Samy <Samy>
2016-12-19 23:55:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/sami/projet_mobile_ferreol_ouldali/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sami.projet_mobile_ferreol_ouldali;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
